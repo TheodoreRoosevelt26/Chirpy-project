972b6df9d515e04d7e35a92ce063a05baecc04e7
TheodoreRoosevelt26 <TR.theodore.roosevelt@proton.me>
2024-10-14 22:59:55 +0300

    Chapter 1: assignment 5 - Fileserver & assignment 8 - Serving Images

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	SM := http.NewServeMux()
 	Server := &http.Server{Addr: ":8080", Handler: SM}
+	SM.Handle("/", http.FileServer(http.Dir(".")))
 	err := Server.ListenAndServe()
 	if err != nil {
 		log.Fatal("Error: unable to start server")
