6b171e330197df9a2ec3ac8f7ff486f78c33553d
TheodoreRoosevelt26 <TR.theodore.roosevelt@proton.me>
2024-10-15 09:26:54 +0300

    Chapter 1: assignment 13 - Handler Review

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	SM := http.NewServeMux()
 	Server := &http.Server{Addr: ":8080", Handler: SM}
-	SM.Handle("/", http.FileServer(http.Dir(".")))
+	SM.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	SM.HandleFunc("/healthz", healthzHandler)
 	err := Server.ListenAndServe()
 	if err != nil {
 		log.Fatal("Error: unable to start server")
