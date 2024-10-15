303bdc17d0e63dbb5ea29a0388f40b13aee882a0
TheodoreRoosevelt26 <TR.theodore.roosevelt@proton.me>
2024-10-15 20:58:06 +0300

    Chapter 2: Assignment 1 - Stateful Handlers

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -11,11 +24,28 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	val := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(val))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+}
+
 func main() {
+	apiCfg := &apiConfig{}
 	SM := http.NewServeMux()
 	Server := &http.Server{Addr: ":8080", Handler: SM}
-	SM.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	SM.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
 	SM.HandleFunc("/healthz", healthzHandler)
+	SM.HandleFunc("/metrics", apiCfg.metricsHandler)
+	SM.HandleFunc("/reset", apiCfg.resetHandler)
 	err := Server.ListenAndServe()
 	if err != nil {
 		log.Fatal("Error: unable to start server")
