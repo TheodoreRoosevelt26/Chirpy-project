04a0c4aef7916eabc4f862c73bd0d7ee007a77e4
TheodoreRoosevelt26 <TR.theodore.roosevelt@proton.me>
2024-11-04 16:12:28 +0200

    Chapter 6: Assignment 6 0.4

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -24,7 +26,7 @@ func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 	claims := jwt.RegisteredClaims{
 		Issuer:    "chirpy",
 		IssuedAt:  jwt.NewNumericDate(time.Now()),
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
 		Subject:   userID.String(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims(claims))
@@ -36,5 +38,33 @@ func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 }
 
 func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
-	// TO DO
+	claims := &jwt.RegisteredClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	subject, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return subject, nil
+}
+
+func GetBearerToken(headers http.Header) (string, error) {
+	check := []rune{'B', 'e', 'a', 'r', 'e', 'r', ' '}
+	bearer := headers.Get("Authorization")
+	if len(bearer) < 8 {
+		return "", fmt.Errorf("401 unauthorized")
+	}
+	partBearer := bearer[:7]
+	runes := []rune(partBearer)
+	for i := 0; i < len(check); i++ {
+		if check[i] != runes[i] {
+			return "", fmt.Errorf("401 unauthorized")
+		}
+	}
+	stringToken := bearer[7:]
+	return stringToken, nil
 }
